0c9f1cdfd570e24e875d36bf89fdd82fac544190
Nafiue297 <nafiuehaque3@gmail.com>
2026-08-16 23:10:50 +0600

    Time: 0 ms (100%), Space: 8.7 MB (34.99%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+    int cnt=0;
+    int n=s.size();
+    for(int i=0; i<n;)
+    {
+        if(s[i]=='X')
+        {
+            cnt++;
+            i+=3;
+        }else i++;
+       
+    }
+    return cnt;
+    }
+};
