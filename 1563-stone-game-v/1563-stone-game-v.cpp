931143cd66d5bfd419e2d611a09e18fcf3593eb3
Nafiue297 <nafiuehaque3@gmail.com>
2026-08-17 23:03:38 +0600

    Time: 436 ms (50.71%), Space: 27.7 MB (33.99%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.cpp
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -5,27 +5,27 @@ int solve(int l, int r, vector<vector<int>>&dp, vector<int>&pref)
     if(l>=r)return 0;
     if(dp[l][r]!=-1)return dp[l][r];
     int score=0;
-    for(int mid=l; mid<=r-1; mid++)
+    for(int i=l; i<=r-1; i++)
     {
-        int left=pref[mid]-(l-1>=0?pref[l-1]:0);
-        int right=pref[r]-pref[mid];
-      if(left<right) score=max(score,left+solve(l,mid,dp,pref));
- else if(left>right) score=max(score,right+solve(mid+1,r,dp,pref));
- else{
-    int lscore=max(score,left+solve(l,mid,dp,pref));
-    int rscore=max(score,right+solve(mid+1,r,dp,pref));
-    score=max(lscore,rscore);
-   }   
-  
+        int left=pref[i]-(l-1>=0?pref[l-1]:0);
+        int right=pref[r]-pref[i];
+    if(left<right)score=max(score,left+solve(l,i,dp,pref));
+    else if(left>right)score=max(score,right+solve(i+1,r,dp,pref));
+    else
+    {
+        int lsum=left+solve(l,i,dp,pref);
+        int rsum=right+solve(i+1,r,dp,pref);
+    score=max(lsum,rsum);
     }
- return dp[l][r]=score;
+    }
+    return dp[l][r]=score;
 }
     int stoneGameV(vector<int>& stoneValue) {
-     int n=stoneValue.size();
-     vector<int>pref(n,0);
-     pref[0]=stoneValue[0];
-     for(int i=1; i<n; i++)pref[i]=pref[i-1]+stoneValue[i];
-     vector<vector<int>>dp(n,vector<int>(n,-1));
-     return solve(0,n-1,dp,pref);
+      int n=stoneValue.size();
+      vector<int>pref(n,0);
+      pref[0]=stoneValue[0];
+      for(int i=1; i<n; i++)pref[i]=pref[i-1]+stoneValue[i];
+      vector<vector<int>>dp(n,vector<int>(n,-1));
+      return solve(0, n-1, dp, pref);  
     }
 };
