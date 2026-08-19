654fafb32651fbdf664de9ef367bf2cac51d8915
Nafiue297 <nafiuehaque3@gmail.com>
2026-08-20 00:39:47 +0600

    Time: 24 ms (69.93%), Space: 60.1 MB (73.48%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-     unordered_map<int,unordered_set<int>>mp;
-     for(int i=0; i<reservedSeats.size(); i++)
+        unordered_map<int,int>mp;
+        for(auto &u:reservedSeats)
+        {
+            int row=u[0];
+            int seat=u[1];
+            mp[row]|=(1<<seat);
+        }
+        int res=(n-mp.size())*2;
+        int maska=(1<<2)|(1<<3)|(1<<4)|(1<<5);
+        int maskb=(1<<4)|(1<<5)|(1<<6)|(1<<7);
+        int maskc=(1<<6)|(1<<7)|(1<<8)|(1<<9);
+     for(auto [u,v]:mp)
      {
-        int row=reservedSeats[i][0];
-        int col=reservedSeats[i][1];
-        mp[row].insert(col);
+        bool a=(maska & v)==0;
+        bool b=(maskb &v)==0;
+        bool c=(maskc &v)==0;
+        if(a and c)res+=2;
+        else if(a or b or c)res++;
      }
-     int result=(n-mp.size())*2;
-     for(auto[i,j]:mp)
-     {
-        auto valid=[&](int seats){
-         return j.find(seats)==j.end();
-        };
-        bool a=valid(2) and valid(3) and valid(4) and valid(5);
-        bool b=valid(4) and valid(5) and valid(6) and valid(7);
-        bool c=valid(6) and valid(7) and valid(8) and valid(9);
-        if(a and c)result+=2;
-        else if(a or b or c)result++;
-     }
-     return result;
+     return res;
     }
 };
