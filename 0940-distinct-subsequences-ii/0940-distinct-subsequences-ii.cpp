2bcd94eaca1d015e104beab3eb2d8e3bf894ae20
Nafiue297 <nafiuehaque3@gmail.com>
2026-09-10 10:02:26 +0600

    Time: 0 ms (100%), Space: 10.5 MB (8.11%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -2,40 +2,35 @@ class Solution {
 public:
     int M = 1e9+7;
     int dp[2001];
-    vector<int> prev; //prev[n] = last time when we saw this nth character (1-based indexing)
+    vector<int> prev; 
 
-    int solve(int n) {
-        if(n == 0)
-            return 1;
-
-        if(dp[n] != -1)
-            return dp[n];
-        
-        int total = (2*solve(n-1)) % M;
-
-        if(prev[n] != 0) {
-            int duplicates = solve(prev[n] - 1);
-            total = (total - duplicates + M) % M;
-        }
-
-        return dp[n] = total;
-    }
-
+   
     int distinctSubseqII(string s) {
         int n = s.length();
 
         memset(dp, -1, sizeof(dp));
-        prev.assign(n+1, 0);
+        prev.assign(n, -1);
 
-        vector<int> lastSeen(26, 0);
-        for(int i = 1; i <= n; i++) {
-            int idx = s[i-1] -'a';
+        vector<int> lastSeen(26, -1);
+        for(int i = 0; i < n; i++) {
+            int idx = s[i] -'a';
 
             prev[i] = lastSeen[idx];
             lastSeen[idx] = i;
         }
+        dp[0]=1;
+        for(int i=1; i<=n; i++)
+        {
+            int total=(2*dp[i-1])%M;
+            if(prev[i-1]!=-1)
+            {
+                 int duplicates = dp[prev[i-1]];
+                  total = (total - duplicates + M) % M;
+            }
+            dp[i]=total;
+        }
 
-        return (solve(n) - 1 + M) % M;
+        return (dp[n] - 1 + M) % M;
 
     }
 };
