03cbfa8f43f57569ea2dd9fce17074d8e82e9534
Nafiue297 <nafiuehaque3@gmail.com>
2026-09-13 12:57:14 +0600

    Time: 68 ms (67.86%), Space: 59.1 MB (31.81%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
-    struct State {
-        int wordIndex;
-        int position;
-    };
-
     int numMatchingSubseq(string s, vector<string>& words) {
-        vector<State> bucket[26];
-
-        // Initially, every word is waiting for its first character
-        for (int i = 0; i < words.size(); i++) {
-            bucket[words[i][0] - 'a'].push_back({i, 0});
-        }
-
-        int ans = 0;
-
-        for (char ch : s) {
-            int index = ch - 'a';
-
-            // Take all words currently waiting for this character
-            vector<State> current = move(bucket[index]);
-
-            for (auto state : current) {
-                int wordIndex = state.wordIndex;
-                int position = state.position + 1;
-
-                // The whole word has been matched
-                if (position == words[wordIndex].size()) {
-                    ans++;
-                }
-                else {
-                    // Wait for the next required character
-                    char nextChar = words[wordIndex][position];
-
-                    bucket[nextChar - 'a'].push_back({
-                        wordIndex,
-                        position
-                    });
-                }
+        int n=s.size();
+        int cnt=0;
+        vector<int>prev[26];
+        for(int i=0; i<n; i++) prev[s[i]-'a'].push_back(i);
+        for(auto u:words)
+        {
+            int pre=-1;
+            bool possible=true;
+        for(auto v:u)
+        {
+            vector<int>&ans=prev[v-'a'];
+            auto it=upper_bound(ans.begin(), ans.end(),pre);
+            if(it==ans.end())
+            {
+                possible =false;
+                break;
             }
-        }
+            pre=*it;
+           
 
-        return ans;
+        } if(possible) cnt++;
+        }
+        return cnt;
     }
 };
