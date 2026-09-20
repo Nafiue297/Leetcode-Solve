6fae07a94d529d9f1616bd3a349a5d8c9a451daf
Nafiue297 <nafiuehaque3@gmail.com>
2026-09-20 20:34:10 +0600

    Time: 42 ms (24.53%), Space: 34.7 MB (15.18%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+bool check(string &s, int i, int j, vector<vector<int>>&dp)
+{
+    if(i>j) return true;
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(s[i]==s[j]) return dp[i][j]=check(s, i+1, j-1, dp);
+    return false;
+}
+    int countSubstrings(string s) {
+        int n=s.size();
+        int cnt=0;
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i; j<n; j++)
+            {
+                if(check(s, i, j, dp)) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
